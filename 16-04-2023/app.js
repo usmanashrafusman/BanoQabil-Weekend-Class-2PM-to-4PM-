dc839d075d77370f4f08001cb085b4a039c95739
Muhammad Usman <usman.ashraf033@gmail.com>
2023-04-16 16:58:35 +0500

    Add files via upload

---
+++ 16-04-2023/app.js
@@ -0,0 +1,79 @@
+// // const multiply = (num1, num2) => {
+// //     const multiple = num1 * num2;
+// //     return multiple;
+// // }
+
+// // const add = (num1, num2) => {
+// //     const sum = num1 + num2;
+// //     return sum;
+// // }
+
+// function multiply(num1, num2) {
+//     const multiple = num1 * num2;
+//     if (multiple > 50){
+//         return multiple+10;
+//     }
+//         return multiple;  
+
+// }
+
+// function add(num1, num2) {
+//     const sum = num1 + num2;
+//     return sum;
+// }
+
+
+// const number1 = multiply(10, 6);
+// console.log(number1 , "Mutiple")
+// const number2 = add(number1, 10);
+// console.log(number2, "Outside");
+
+// const weekDays = ["Sun", "Mon", "Tue", "Wed", "Thus", "Friday", "Sat"];
+// const day = weekDays[new Date().getDay()];
+
+// switch (day) {
+//     case "Sun":
+//         alert("Whoopee");
+//         break;
+//     case "Sat":
+//         alert("Whoopee");
+//         break;
+//     case "Fri":
+//         alert("Today is Jummah");
+//         break;
+//     case "Thu":
+//         alert("Weekend is near");
+//         break;
+//     case "Wed":
+//         alert("I am tired of boring wednesday");
+//         break;
+//     case "Tue":
+//         alert("Today is tuesday");
+//         break;
+//     case "Mon":
+//         alert("Weekend is over");
+//         break;
+//     default:
+//         alert("Shoot me now!");
+// }
+
+// const arrOfEven = [];
+// let number = 0;
+// while (5 > arrOfEven.length) {
+//     number++;
+//     if (number % 2 === 0) {
+//         arrOfEven.push(number)
+//     }
+// }
+// console.log(arrOfEven);
+
+
+// var i = 0;
+// do {
+//     alert(i);
+//     i++;
+// } while (i < 0);
+
+function sayHi() {
+    alert("Hello")
+}
